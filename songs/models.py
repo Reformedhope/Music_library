5fcdc0b93384a839f22345b2c36e1cae7afc4c76
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-08 18:44:49 -0400

    created a model for the song table and added a likes feild

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#This is where we are going to create representations of our table in the database. which is were we will define them
+#creating the model first is called the code first approch with backend, Than the table will create auto based on the class. 
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField
+    genre = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# This is where we are going to define all the function that  the outside world can interact with.
+#
