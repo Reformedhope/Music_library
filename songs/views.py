1b9a3dc3ed70fe14f5b9bd2d2f83abb60e635af0
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-08 19:46:11 -0400

    Updated Urls + Created View function + Created Song App urls FIle

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # This is where we are going to define all the function that  the outside world can interact with.
 #
+
+@api_view(['GET'])   # inside the list is wshere we define what http method type are allow to be executed
+def song_list (request):
+    
+    return Response('ok')
