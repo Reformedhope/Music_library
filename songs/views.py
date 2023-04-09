599732d6a8c6a9f43e872c717ae74015decaa46b
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-08 21:51:51 -0400

    Added PostMan collection

--- songs/views.py
+++ songs/views.py
@@ -34,6 +34,9 @@ def song_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     
  
