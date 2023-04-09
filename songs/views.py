e09dda20b3592721f82625edd0f427e6b4aa0bed
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-08 20:00:39 -0400

    updated Views File to allow for pull requests

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
 # This is where we are going to define all the function that  the outside world can interact with.
 #
 
 @api_view(['GET'])   # inside the list is wshere we define what http method type are allow to be executed
 def song_list (request):
+   
+    songs = Song.objects.all()
+
+    serializer = SongSerializer(songs, many=True)
     
-    return Response('ok')
+
+    return Response(serializer.data)
