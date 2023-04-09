a9a73d2dbcf32000e3bb8993ea30bb6d99692fd1
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-08 20:45:23 -0400

    Registered Song model with Admin Site +created song lists using admin site + created POST functioon + created song using Post Request +updated Post function

--- songs/views.py
+++ songs/views.py
@@ -1,16 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SongSerializer
 from .models import Song
 # This is where we are going to define all the function that  the outside world can interact with.
 #
 
-@api_view(['GET'])   # inside the list is wshere we define what http method type are allow to be executed
+@api_view(['GET', 'POST'])   # inside the list is wshere we define what http method type are allow to be executed
 def song_list (request):
-   
-    songs = Song.objects.all()
 
-    serializer = SongSerializer(songs, many=True)
+    if request.method =='GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
     
-
-    return Response(serializer.data)
+    elif request.method =='POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
