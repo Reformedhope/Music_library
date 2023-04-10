b042247a60f12948ba9acb367ea5001dae4abe9b
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-04-10 09:19:45 -0400

    updated the serializer file + the models file to allow for a release date section

--- songs/models.py
+++ songs/models.py
@@ -8,7 +8,7 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField
+    release_date = models.DateField(null=True)
     genre = models.CharField(max_length=255)
     likes = models.IntegerField()
     
--- songs/serializers.py
+++ songs/serializers.py
@@ -4,4 +4,4 @@ from.models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'artist', 'album', 'genre', 'likes' ]
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre', 'likes' ]
